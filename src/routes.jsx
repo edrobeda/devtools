6b69137a53afacbf9b7449f869cf075f82c421cb
edrobeda <edrobeda@gmail.com>
2026-08-17 22:39:25 +0000

    Adiciona Cheat Sheet de Java (/references/java-cheatsheet)

--- src/routes.jsx
+++ src/routes.jsx
@@ -453,6 +453,7 @@ import NodejsCheatsheetPage from './pages/NodejsCheatsheetPage'
 import PostgresqlCheatsheetPage from './pages/PostgresqlCheatsheetPage'
 import GoCheatsheetPage from './pages/GoCheatsheetPage'
 import RustCheatsheetPage from './pages/RustCheatsheetPage'
+import JavaCheatsheetPage from './pages/JavaCheatsheetPage'
 
 const router = createBrowserRouter([
   {
@@ -904,6 +905,7 @@ const router = createBrowserRouter([
       { path: 'references/postgresql-cheatsheet', element: <PostgresqlCheatsheetPage /> },
       { path: 'references/go-cheatsheet', element: <GoCheatsheetPage /> },
       { path: 'references/rust-cheatsheet', element: <RustCheatsheetPage /> },
+      { path: 'references/java-cheatsheet', element: <JavaCheatsheetPage /> },
       { path: 'tools/bankers-algorithm-simulator', element: <BankersAlgorithmSimulatorPage /> },
       { path: 'tools/memory-allocation-simulator', element: <MemoryAllocationSimulatorPage /> },
       { path: 'tools/raft-simulator', element: <RaftSimulatorPage /> },
